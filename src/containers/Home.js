cf0a604e3b11f48cc3e48c45f8c848acbd171972
Thomas Frost <tom@fr0.st>
2020-10-01 10:33:36 -0700

    Updated the lander with login/signup buttons

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useAppContext } from "../libs/contextLib";
@@ -55,14 +56,22 @@ export default function Home() {
     );
   }
 
-  function renderLander() {
-    return (
-      <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+function renderLander() {
+  return (
+    <div className="lander">
+      <h1>Scratch</h1>
+      <p>A simple note taking app</p>
+      <div>
+        <Link to="/login" className="btn btn-info btn-lg">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn-success btn-lg">
+          Signup
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
   function renderNotes() {
     return (
